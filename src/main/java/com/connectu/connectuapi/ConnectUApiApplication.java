4c4fe73df269aa4f03a6cd0a8d896c38bee59a97
yang <yang87924@gmail.com>
2023-05-29 23:57:59 +0800

    feat: 🎸 init

---
+++ src/main/java/com/connectu/connectuapi/ConnectUApiApplication.java
@@ -0,0 +1,13 @@
+package com.connectu.connectuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectUApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectUApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/connectu/connectuapi/ConnectUApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectu.connectuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectUApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
