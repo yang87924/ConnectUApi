8a1183a075905c834b30d3f12dfc097dbf0e183b
SeeIetw <star159357tw@gmail.com>
2023-07-10 14:21:04 +0800

    chat finished

--- src/main/java/com/connectu/connectuapi/config/MyGlobalCORSConfig.java
+++ src/main/java/com/connectu/connectuapi/config/MyGlobalCORSConfig.java
@@ -17,10 +17,12 @@ public class MyGlobalCORSConfig {
                 registry.addMapping("/**") // 允许跨域访问的路径
                         .allowedOrigins("http://localhost:6999",
                                 "https://apifox.com/apidoc/shared-c32e3a6f-756f-43e3-aed6-5408aa8d0247/api-88082215",
-                                "http://localhost:3000",
-                                "http://10.0.104.210:6999",
-                                "http://10.0.104.210",
-                                "http://forum.connectu.life") // 允许跨域访问的源
+                                "http://43.207.68.213:6999",
+                                "http://43.207.68.213",
+                                "http://connectu.life",
+                                "https://connectu.life",
+                                "http://forum.connectu.life",
+                                "https://forum.connectu.life") // 允许跨域访问的源
           //             .allowedOrigins("*")
                         .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE") // 允许请求的方法
                         .maxAge(168000) // 预检间隔时间
